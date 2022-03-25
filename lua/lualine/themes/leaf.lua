d3023a779180c98c1108b35bdd02f43b38b06df1
Daniel Schwabeneder <daschw@disroot.org>
2022-03-25 23:14:59 +0100

    simplify hlgroups and clarify colors

--- lua/lualine/themes/leaf.lua
+++ lua/lualine/themes/leaf.lua
@@ -3,35 +3,35 @@ local colors = require("leaf.colors").setup()
 local leaf = {}
 
 leaf.normal = {
-    a = { bg = colors.bg2, fg = colors.fg0 },
-    b = { bg = colors.bg1, fg = colors.fg1 },
-    c = { bg = colors.bg0, fg = colors.bg2 },
+    a = { bg = colors.bg_dim, fg = colors.fg_normal },
+    b = { bg = colors.bg_soft, fg = colors.fg_soft },
+    c = { bg = colors.bg_normal, fg = colors.bg_dim },
 }
 
 leaf.insert = {
-    a = { bg = colors.green0, fg = colors.bg0 },
-    b = { bg = colors.bg0, fg = colors.green0 },
+    a = { bg = colors.green_soft, fg = colors.bg_normal },
+    b = { bg = colors.bg_normal, fg = colors.green_soft },
 }
 
 leaf.command = {
-    a = { bg = colors.yellow0, fg = colors.bg0 },
-    b = { bg = colors.bg0, fg = colors.yellow0 },
+    a = { bg = colors.yellow_soft, fg = colors.bg_normal },
+    b = { bg = colors.bg_normal, fg = colors.yellow_soft },
 }
 
 leaf.visual = {
-    a = { bg = colors.purple0, fg = colors.bg0 },
-    b = { bg = colors.bg0, fg = colors.purple0 },
+    a = { bg = colors.purple_soft, fg = colors.bg_normal },
+    b = { bg = colors.bg_normal, fg = colors.purple_soft },
 }
 
 leaf.replace = {
-    a = { bg = colors.red0, fg = colors.bg0 },
-    b = { bg = colors.bg0, fg = colors.red0 },
+    a = { bg = colors.red_soft, fg = colors.bg_normal },
+    b = { bg = colors.bg_normal, fg = colors.red_soft },
 }
 
 leaf.inactive = {
-    a = { bg = colors.bg0, fg = colors.blue0 },
-    b = { bg = colors.bg0, fg = colors.bg1 },
-    c = { bg = colors.bg0, fg = colors.bg2 },
+    a = { bg = colors.bg_normal, fg = colors.blue_soft },
+    b = { bg = colors.bg_normal, fg = colors.bg_soft },
+    c = { bg = colors.bg_normal, fg = colors.bg_dim },
 }
 
 if vim.g.leaf_lualine_bold then
